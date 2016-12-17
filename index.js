1430b268a85ad0c89d29dbd2d0a1a9bbd1aafdc3
rotsee <leo_wallentin@hotmail.com>
2016-12-17 12:12:39 +0100

    use async

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 'use strict'
 var restify = require('restify')
 var pug = require('pug')
+var async = require('async')
 var Logger = require('bunyan')
 
 // Routes
@@ -21,6 +22,15 @@ var makeChartData = function (data){
   })
   return chartData
 }
+var callEndpoint = function(client, endpoint, callback){
+  var cb = callback
+  client.get(
+    '/'+endpoint+'?jpp='+process.env.JPP_CODE,
+    function(err, req, res, obj) {
+      cb(err, obj)
+    }
+  )
+}
 var chart = function(req, res, next){
   var res_ = res
   // get data
@@ -28,17 +38,23 @@ var chart = function(req, res, next){
     url: 'http://jpp-inkomstkollen.herokuapp.com',
     version: '*'
   })
-  client.get('/incomes?jpp='+process.env.JPP_CODE, function(err, req, res, obj) {
-    var incomeData = obj
-
-    client.get('/people?jpp='+process.env.JPP_CODE, function(err, req, res, obj) {
-      res_.end(pug.renderFile('views/chart.jade',{
-        income_series: makeChartData(incomeData),
-        people_series: makeChartData(obj)
+  async.parallel(
+    {
+      incomes: function(callback) {
+        callEndpoint(client, "incomes", callback)
+      },
+      people: function(callback) {
+        callEndpoint(client, "people", callback)
+      }
+    },
+    function(err, results) {
+      res_.end(pug.renderFile('views/chart.jade', {
+        income_series: makeChartData(results.incomes),
+        people_series: makeChartData(results.people)
       }))
-    })
+    }
+  ) // async
 
-  })
 }
 
 // Logger
