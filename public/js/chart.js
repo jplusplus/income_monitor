28bbb18a7dd233f4c2b17bf2fab630eeab4574b6
rotsee <leo_wallentin@hotmail.com>
2016-12-17 13:14:57 +0100

    crop xaxis

--- public/js/chart.js
+++ public/js/chart.js
@@ -1,43 +1,59 @@
 $(function () {
-    Highcharts.chart('container', {
+  var container = $('#container')
+  var income_series = container.data("income_series")
+  var people_series = container.data("people_series")
+
+  Highcharts.chart('container', {
+    title: {
+      text: 'Intäkter och anställda'
+    },
+    subtitle: {
+      text: 'Fakturerat i snitt per månad (rullande kvartal)'
+    },
+    xAxis: {
+      type: 'datetime',
+      title: {
+        text: 'Månad'
+      },
+      min: income_series[income_series.length - 1][0]-1e9,
+      max: income_series[0][0]+1e9,
+//      startOnTick: true
+    },
+    yAxis: [
+      {
         title: {
-            text: 'Intäkter och anställda'
+          text: 'SEK'
         },
-        subtitle: {
-            text: 'Fakturerat i snitt per månad (rullande kvartal)'
+        min: 0
+      }, {
+        title: {
+          text: 'tjänster'
         },
-        xAxis: {
-            type: 'datetime',
-            title: {
-                text: 'Månad'
-            }
-        },
-        yAxis: [
-            {
-                title: {
-                    text: 'SEK'
-                },
-                min: 0
-            }, {
-                title: {
-                    text: 'tjänster'
-                },
-                min: 0,
-                opposite: true
-            }
-        ],
-        tooltip: {
-            shared: true,
-        },
+        min: 0,
+        opposite: true
+      }
+    ],
+    tooltip: {
+        shared: true,
+    },
 
-        plotOptions: {
-            spline: {
-                marker: {
-                    enabled: true
-                }
+    plotOptions: {
+        spline: {
+            marker: {
+                enabled: true
             }
-        },
+        }
+    },
 
-        series: series
-    });
+    series: [{
+          name: 'Tjänster',
+          type: 'column',
+          yAxis: 1,
+          data: people_series
+        }, {
+          name: 'Fakturerat',
+          type: 'spline',
+          data: income_series
+        }]
+  });
 });
